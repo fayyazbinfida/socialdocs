272bbb170dea727315f4aadd844695608fbdb167
fayyazbinfida <khanfayyaz45@gmail.com>
2023-12-30 19:24:31 +0500

    home page created

--- index.php
+++ index.php
@@ -3,36 +3,77 @@
     include('sidebar.php');
 ?>
 <main class="page">
-        <div class="theme-default-content content__default">
-          <h1 id="introduction"><a href="#introduction" class="header-anchor">#</a> Introduction</h1>
-          <p>👋 Create your first Chatbot on the Smart ChatBot platform. To begin with, have a quick look at the platform in a 2
-            minutes video:</p> <iframe width="949" height="534" src="https://www.youtube.com/embed/BHI_wcNtfdc"
-            title="YouTube video player" frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen="allowfullscreen"></iframe>
-          <p>In this section, you will learn</p>
-          <ul>
-            <li>How to register</li>
-            <li>Platform overview</li>
-            <li>Workspaces &amp; members</li>
-            <li>How to set up your bot in each channel</li>
-            <li>Integrations overview</li>
-            <li>Ecommerce system overview</li>
-            <li>How to upgrade your account</li>
-          </ul>
-        </div>
-        <footer class="page-edit"><!---->
-          <div class="last-updated"><span class="prefix">Last Updated:</span> <span class="time">6/6/2022, 11:34:37
-              AM</span></div>
-        </footer>
-        <div class="page-nav">
-          <p class="inner"><!----> <span class="next"><a href="register.php">
-                Registration
-              </a>
-              →
-            </span></p>
-        </div>
-      </main>
+  <div class="theme-default-content content__default">
+    <h1 id="introduction">
+      <a href="#introduction" class="header-anchor">#</a>
+      Connecting Social Media Accounts in Smart Social: A Step-by-Step Guide
+    </h1>
+    <p>
+      Are you ready to supercharge your social media presence with Smart Social? Great! We've put together a simple guide to help you seamlessly connect your social media accounts and unlock the full potential of our platform. Follow these easy steps:
+    </p>
+    <h1 id="introduction">
+      <a href="#introduction" class="header-anchor">#</a>
+      Step 1: Log into Your Smart Social Account
+    </h1>
+    <p>
+      Begin by logging into your Smart Social account. Ensure you use the same browser for both Smart Social and the social media account you wish to connect.
+    </p>
+    <h1 id="introduction">
+      <a href="#introduction" class="header-anchor">#</a>
+      Step 2: Navigate to "Accounts" and Select "Connect Account"
+    </h1>
+    <p>
+      Once logged in, head over to the "Accounts" section. Here, you'll find the option to "Connect Account." Click on it to initiate the process.
+    </p>
+    <h1 id="introduction">
+      <a href="#introduction" class="header-anchor">#</a>
+      Step 3: Choose Your Social Media Platform
+    </h1>
+    <p>
+      A list of various social media platforms will appear. Select the platform (e.g., Facebook, Instagram) for the account you want to connect. Be sure that the account is logged in on the same browser for a smooth connection process.
+    </p>
+    <h1 id="introduction">
+      <a href="#introduction" class="header-anchor">#</a>
+      Step 4: Log into Your Social Media Account
+    </h1>
+    <p>
+      After selecting the platform, you'll be prompted to log into your social media account. This step is crucial for Smart Social to establish a secure and authorized connection.
+    </p>
+    <h1 id="introduction">
+      <a href="#introduction" class="header-anchor">#</a>
+      Step 5: Success! Your Account is Connected
+    </h1>
+    <p>
+      Once the login is successful, you'll receive a confirmation that your social media account is now connected. Congratulations! You're now ready to explore the full range of features Smart Social has to offer.
+    </p>
+    <h1 id="introduction">
+      <a href="#introduction" class="header-anchor">#</a>
+      Step 6: Explore "Manage Accounts" for Further Control
+    </h1>
+    <p>
+      To manage your connected accounts, navigate to the "Manage Accounts" section. Here, you can see a clear list of your connected accounts and access additional options. From here, you can post content, view analytics, and take full control of your social media strategy.
+    </p>
+    <p>
+      Connecting your social media accounts with Smart Social opens up a world of possibilities, from efficient content scheduling to in-depth analytics. Now you're equipped to make the most of your social media presence!
+    </p>
+      If you encounter any issues during the process, don't hesitate to reach out to our support team for assistance. Happy connecting!
+    </p>
+  </div>
+  <footer class="page-edit">
+    <div class="last-updated">
+      <span class="prefix">Last Updated:</span>
+      <span class="time">6/6/2022, 11:34:37 AM</span>
+    </div>
+  </footer>
+  <div class="page-nav">
+    <p class="inner">
+      <span class="next">
+        <a href="register.php">Registration</a>
+        →
+      </span>
+    </p>
+  </div>
+</main>
 <?php
 include('footer.php');
 ?>
--- sidebar.php
+++ sidebar.php
@@ -1,228 +1,74 @@
 <div class="sidebar-mask"></div>
 <aside class="sidebar">
-        <ul class="sidebar-links">
+  <ul class="sidebar-links">
+    <li>
+      <section class="sidebar-group depth-0">
+        <p class="sidebar-heading open">
+          <span>Connecting Social Media Accounts in Smart Social</span>
+        </p>
+        <ul class="sidebar-links sidebar-group-items">
           <li>
-            <section class="sidebar-group depth-0">
-              <p class="sidebar-heading open"><span>Guide</span> <!----></p>
-              <ul class="sidebar-links sidebar-group-items">
-                <li><a href="index.php" aria-current="page" class="active sidebar-link">Introduction</a></li>
-                <li><a href="register.php" class="sidebar-link">Registration</a></li>
-                <li><a href="components.php" class="sidebar-link">Platform Overview</a>
-                  <ul class="sidebar-sub-headers">
-                    <li class="sidebar-sub-header">
-                      <a href="components.php#overview" class="sidebar-link">Overview</a>
-                    </li>
-                    <li class="sidebar-sub-header">
-                      <a href="components.php#sidebar" class="sidebar-link">Sidebar</a>
-                    </li>
-                  </ul>
-                </li>
-                <li><a href="account.php" aria-current="page" class="active sidebar-link">Workspaces &amp; Members</a>
-                  <ul class="sidebar-sub-headers">
-                    <li class="sidebar-sub-header"><a href="account.php#workspace-limitation"
-                        class="sidebar-link">Workspace Limitation</a></li>
-                    <li class="sidebar-sub-header"><a href="account.php#workspace-settings"
-                        class="sidebar-link">Workspace Settings</a></li>
-                    <li class="sidebar-sub-header"><a href="account.php#user-settings-timezone"
-                        class="sidebar-link">User Settings (Timezone)</a></li>
-                    <li class="sidebar-sub-header"><a href="account.php#members-and-roles" class="sidebar-link">Members
-                        and Roles</a></li>
-                  </ul>
-                </li>
-              </ul>
-            </section>
+            <a href="index.php" aria-current="page" class="active sidebar-link">Step 01</a>
+            <ul class="sidebar-sub-headers">
+              <li class="sidebar-sub-header">
+                <a href="account.php#workspace-limitation" class="sidebar-link">
+                  Log into Your Smart Social Account
+                </a>
+              </li>
+            </ul>
           </li>
           <li>
-            <section class="sidebar-group depth-0">
-                <p class="sidebar-heading"><span>Setup</span></p>
-              <ul class="sidebar-links sidebar-group-items">
-                <li><a href="setup-create.php" class="sidebar-link">Connect to Channels</a>
-                <ul class="sidebar-sub-headers">
-                    <li class="sidebar-sub-header">
-                      <a href="setup-create.php#omni-bot" class="sidebar-link">Omni Bot</a>
-                    </li>
-                    <li class="sidebar-sub-header">
-                      <a href="setup-create.php#facebook-bot" class="sidebar-link">Facebook Bot</a>
-                    </li>
-                    <li class="sidebar-sub-header">
-                      <a href="setup-create.php#instagram-bot" class="sidebar-link">Instagram Bot</a>
-                    </li>
-                    <li class="sidebar-sub-header">
-                      <a href="setup-create.php#telegram-bot" class="sidebar-link">Telegram Bot</a>
-                    </li>
-                    <li class="sidebar-sub-header">
-                      <a href="setup-create.php#slack-bot" class="sidebar-link">Slack Bot</a>
-                    </li>
-                    <li class="sidebar-sub-header">
-                      <a href="setup-create.php#wechat-bot" class="sidebar-link">WeChat Bot</a>
-                    </li>
-                    <li class="sidebar-sub-header">
-                      <a href="setup-create.php#whatsapp-sms-voice-bot" class="sidebar-link">WhatsApp, SMS &amp; Voice Bot</a>
-                    </li>
-                    <li class="sidebar-sub-header">
-                      <a href="setup-create.php#whatsapp-360dialog-sandbox" class="sidebar-link">WhatsApp + 360Dialog Sandbox</a>
-                    </li>
-                    <li class="sidebar-sub-header">
-                      <a href="setup-create.php#google-my-business" class="sidebar-link">Google My Business</a>
-                    </li>
-                    <li class="sidebar-sub-header">
-                      <a href="setup-create.php#vk-bot" class="sidebar-link">VK Bot</a>
-                    </li>
-                    <li class="sidebar-sub-header">
-                      <a href="setup-create.php#intercom-bot" class="sidebar-link">Intercom Bot</a>
-                    </li>
-                  </ul>
-                </li>
-                <li><a href="whatsapp-api.php" class="sidebar-link">WhatsApp + 360Dialog ($50/month)</a>
-                <ul class="sidebar-sub-headers">
-                    <li class="sidebar-sub-header">
-                      <a href="whatsapp-api.php#checklist" class="sidebar-link">Checklist</a>
-                    </li>
-                </ul>
-                </li>
-                <li><a href="cloudapi.php" class="sidebar-link">WhatsApp Cloud API</a>
-                  <ul class="sidebar-sub-headers">
-                    <li class="sidebar-sub-header">
-                      <a href="cloudapi.php#set-up-facebook-app" class="sidebar-link">Set up Facebook App</a>
-                    </li>
-                    <li class="sidebar-sub-header">
-                      <a href="cloudapi.php#get-your-webhook-from-uchat" class="sidebar-link">Get your Webhook from Smart ChatBot</a>
-                    </li>
-                    <li class="sidebar-sub-header">
-                      <a href="cloudapi.php#start-with-test-numbers" class="sidebar-link">Start with test numbers</a>
-                    </li>
-                    <li class="sidebar-sub-header">
-                      <a href="cloudapi.php#build-whatsapp-bot-with-testing-whatsapp-number" class="sidebar-link">Build WhatsApp bot with testing WhatsApp number</a>
-                    </li>
-                    <li class="sidebar-sub-header">
-                      <a href="cloudapi.php#supported-message-types" class="sidebar-link">Supported message types</a>
-                    </li>
-                    <li class="sidebar-sub-header">
-                      <a href="cloudapi.php#template-message" class="sidebar-link">Template message</a>
-                    </li>
-                    <li class="sidebar-sub-header">
-                      <a href="cloudapi.php#how-to-use-template-message" class="sidebar-link">How to use Template message</a>
-                    </li>
-                    <li class="sidebar-sub-header">
-                      <a href="cloudapi.php#whatsapp-native-ecommerce-product-catalog-message" class="sidebar-link">WhatsApp Native Ecommerce - Product catalog message</a>
-                    </li>
-                    <li class="sidebar-sub-header">
-                      <a href="cloudapi.php#add-your-own-business-phone-number" class="sidebar-link">Add your own business phone number</a>
-                    </li>
-                    <li class="sidebar-sub-header">
-                      <a href="cloudapi.php#switch-the-flow-from-the-testing-phone-number-to-your-business-phone-number" class="sidebar-link">Switch the flow from the Testing phone number to your business phone number</a>
-                    </li>
-                    <li class="sidebar-sub-header">
-                      <a href="cloudapi.php#pricing" class="sidebar-link">Pricing</a>
-                    </li>
-                    <li class="sidebar-sub-header">
-                      <a href="cloudapi.php#business-verification-limits"
-                        class="sidebar-link">Business verification limits</a>
-                      </li>
-                    <li class="sidebar-sub-header">
-                      <a href="cloudapi.php#how-to-get-your-business-verified" class="sidebar-link">How to get your business verified?</a>
-                    </li>
-                    <li class="sidebar-sub-header">
-                      <a href="cloudapi.php#display-name-guidelines" class="sidebar-link">Display name guidelines</a>
-                    </li>
-                    <li class="sidebar-sub-header">
-                      <a href="cloudapi.php#faq" class="sidebar-link">FAQ:</a>
-                    </li>
-                  </ul>
-                </li>
-                <li><a href="whitelabel.php" class="sidebar-link">White Label Settings</a>
-                <ul class="sidebar-sub-headers">
-                    <li class="sidebar-sub-header">
-                      <a href="whitelabel.php#revenue-sharing"class="sidebar-link">Revenue sharing</a>
-                    </li>
-                    <li class="sidebar-sub-header">
-                      <a href="whitelabel.php#set-up-your-custom-domain" class="sidebar-link">Set up your custom domain</a>
-                    </li>
-                    <li class="sidebar-sub-header">
-                      <a href="whitelabel.php#hide-uchat-brand" class="sidebar-link">Hide Smart ChatBot brand</a>
-                    </li>
-                    <li class="sidebar-sub-header">
-                      <a href="whitelabel.php#customize-logos" class="sidebar-link">Customize logos</a>
-                    </li>
-                    <li class="sidebar-sub-header">
-                      <a href="whitelabel.php#customize-links" class="sidebar-link">Customize links</a>
-                    </li>
-                    <li class="sidebar-sub-header">
-                      <a href="whitelabel.php#set-up-your-own-support" class="sidebar-link">Set up your own support</a>
-                    </li>
-                    <li class="sidebar-sub-header">
-                      <a href="whitelabel.php#send-from-your-own-email-address" class="sidebar-link">Send from your own email address</a>
-                    </li>
-                    <li class="sidebar-sub-header">
-                      <a href="whitelabel.php#receive-slack-notifications" class="sidebar-link">Receive slack notifications</a>
-                    </li>
-                    <li class="sidebar-sub-header">
-                      <a href="whitelabel.php#set-up-your-own-terms-of-conditions" class="sidebar-link">Set up your own Terms of conditions</a>
-                    </li>
-                    <li class="sidebar-sub-header">
-                      <a href="whitelabel.php#workspaces" class="sidebar-link">Workspaces</a>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-            </section>
+            <a href="index.php" aria-current="page" class="active sidebar-link">Step 02</a>
+            <ul class="sidebar-sub-headers">
+              <li class="sidebar-sub-header">
+                <a href="account.php#workspace-limitation" class="sidebar-link">
+                  Navigate to "Accounts" and Select "Connect Account"
+                </a>
+              </li>
+            </ul>
           </li>
           <li>
-            <section class="sidebar-group depth-0">
-              <p class="sidebar-heading"><span>Dashboard</span> <!----></p>
-                <ul class="sidebar-links sidebar-group-items">
-                  <li><a href="integration.php" class="sidebar-link">Integrations</a></li>
-                  <li><a href="ecommerce.php" class="sidebar-link">Ecommerce</a>
-                  <ul class="sidebar-sub-headers">
-                    <li class="sidebar-sub-header">
-                      <a href="ecommerce.php#built-in-ecommerce-system" class="sidebar-link">Built-in Ecommerce System</a>
-                    </li>
-                    <li class="sidebar-sub-header">
-                      <a href="ecommerce.php#single-buy-button" class="sidebar-link">Single Buy Button</a>
-                    </li>
-                  </ul>
-                  </li>
-                  <li><a href="flow-template.php" class="sidebar-link">Bot Templates</a>
-                  <ul class="sidebar-sub-headers">
-                    <li class="sidebar-sub-header">
-                      <a href="flow-template.php#create-flows-bots" class="sidebar-link">Create Flows/Bots</a>
-                    </li>
-                    <li class="sidebar-sub-header">
-                      <a href="flow-template.php#create-templates" class="sidebar-link">Create Templates</a>
-                    </li>
-                    <li class="sidebar-sub-header">
-                      <a href="flow-template.php#edit-templates" class="sidebar-link">Edit Templates</a>
-                    </li>
-                  </ul>
-                  </li>
-                  <li><a href="email-template.php" class="sidebar-link">Email Templates</a>
-                  <ul class="sidebar-sub-headers">
-                    <li class="sidebar-sub-header">
-                      <a href="email-template.php#create-template" class="sidebar-link">Create Template</a>
-                    </li>
-                    <li class="sidebar-sub-header">
-                      <a href="email-template.php#use-template" class="sidebar-link">Use Template</a>
-                    </li>
-                  </ul>
-                  </li>
-                  <li><a href="billing.php" class="sidebar-link">Billing (Upgrade/Redeem)</a>
-                    <ul class="sidebar-sub-headers">
-                      <li class="sidebar-sub-header">
-                        <a href="billing.php#subscription" class="sidebar-link">Subscription</a>
-                      </li>
-                      <li class="sidebar-sub-header">
-                        <a href="billing.php#redeem" class="sidebar-link">Redeem</a>
-                      </li>
-                    <li class="sidebar-sub-header">
-                      <a href="billing.php#payment-method" class="sidebar-link">Payment Method</a>
-                    </li>
-                    <li class="sidebar-sub-header">
-                      <a href="billing.php#invoices" class="sidebar-link">Invoices</a>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-            </section>
+            <a href="index.php" aria-current="page" class="active sidebar-link">Step 03</a>
+            <ul class="sidebar-sub-headers">
+              <li class="sidebar-sub-header">
+                <a href="account.php#workspace-limitation" class="sidebar-link">
+                  Choose Your Social Media Platform
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href="index.php" aria-current="page" class="active sidebar-link">Step 04</a>
+            <ul class="sidebar-sub-headers">
+              <li class="sidebar-sub-header">
+                <a href="account.php#workspace-limitation" class="sidebar-link">
+                  Log into Your Social Media Account
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href="index.php" aria-current="page" class="active sidebar-link">Step 05</a>
+            <ul class="sidebar-sub-headers">
+              <li class="sidebar-sub-header">
+                <a href="account.php#workspace-limitation" class="sidebar-link">
+                  Success! Your Account is Connected
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href="index.php" aria-current="page" class="active sidebar-link">Step 06</a>
+            <ul class="sidebar-sub-headers">
+              <li class="sidebar-sub-header">
+                <a href="account.php#workspace-limitation" class="sidebar-link">
+                  Explore "Manage Accounts" for Further Control
+                </a>
+              </li>
+            </ul>
           </li>
         </ul>
-      </aside>
+      </section>
+    </li>
+  </ul>
+</aside>
