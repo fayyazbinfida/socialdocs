7e6b38dea9a379f0c6ae2997fac7c4be596ee524
fayyazbinfida <khanfayyaz45@gmail.com>
2023-12-30 18:01:30 +0500

    checking

--- header.php
+++ header.php
@@ -181,7 +181,7 @@ session_start();
                         <path fill="currentColor" d="M436 124H12c-6.627 0-12-5.373-12-12V80c0-6.627 5.373-12 12-12h424c6.627 0 12 5.373 12 12v32c0 6.627-5.373 12-12 12zm0 160H12c-6.627 0-12-5.373-12-12v-32c0-6.627 5.373-12 12-12h424c6.627 0 12 5.373 12 12v32c0 6.627-5.373 12-12 12zm0 160H12c-6.627 0-12-5.373-12-12v-32c0-6.627 5.373-12 12-12h424c6.627 0 12 5.373 12 12v32c0 6.627-5.373 12-12 12z"></path>
                     </svg>
                 </div>
-                <a href="index.php" aria-current="page" class="home-link router-link-exact-active router-link-active"><img src="logo.png" alt="Help Center" class="logo">
+                <a href="index.php" aria-current="page" class="home-link router-link-exact-active router-link-active"><img src="ooo.png" alt="Help Center" class="logo">
                     <span class="site-name can-hide">Help Center</span>
                 </a>
                 <div class="links">
