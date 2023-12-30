28bff188014fc4ae1559820d403cffafba4bd4e9
fayyazbinfida <khanfayyaz45@gmail.com>
2023-12-30 17:30:25 +0500

    Updated to BrixSocial

--- footer.php
+++ footer.php
@@ -1,16 +1,12 @@
-<div class="footer">
-          Licensed | Copyright © 2023-present
-        </div>
-      </main>
-    </div>
+</div>
     <div class="global-ui"><!----><!----></div>
   </div>
-  <!-- <script src="assets/js/app.c0e6e995.js" defer></script>
-  <script src="assets/js/7.c4a9717a.js" defer></script>
-  <script src="assets/js/121.d11d34d8.js" defer></script>
-  <script src="assets/js/92.7e42704c.js" defer></script> -->
+  <!-- <script src="../assets/js/app.c0e6e995.js" defer></script>
+  <script src="../assets/js/7.c4a9717a.js" defer></script>
+  <script src="../assets/js/136.d705b759.js" defer></script>
+  <script src="../assets/js/92.7e42704c.js" defer></script> -->
 </body>
 
-<!-- Mirrored from docs.uchat.com.au/ by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 27 Dec 2023 09:41:27 GMT -->
+<!-- Mirrored from docs.uchat.com.au/guide/ by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 27 Dec 2023 09:41:30 GMT -->
 
 </html>
