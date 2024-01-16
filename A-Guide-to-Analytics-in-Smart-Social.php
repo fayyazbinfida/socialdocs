115f7856288be802d324a3b9a3d734e61a9a5bca
Ehtasham-Saleem <ehtisham9048@gmail.com>
2024-01-16 13:10:24 +0500

    A Guide to Analytics in Smart Social shots done

--- A-Guide-to-Analytics-in-Smart-Social.php
+++ A-Guide-to-Analytics-in-Smart-Social.php
@@ -18,6 +18,7 @@
             <li><strong>1.1 Navigate to "Analytics":</strong> Start by clicking on the "Analytics" option in Smart Social's menu.</li>
             <li><strong>1.2 Select Social Media Platform:</strong> Choose the social media platform you want to analyze. For example, if you select "Facebook," you'll get insights specifically for Facebook.</li>
         </ol>
+        <img src="assets/img/Accessanalytics.jpg" alt="Login Image">
 
         <h1 id="step02">
             <a href="#step02" class="header-anchor">#</a>
@@ -27,6 +28,7 @@
             <li><strong>2.1 Choose a Page:</strong> After selecting a platform, choose a specific page or account you want to analyze.</li>
             <li><strong>2.2 Hit "Analyze":</strong> Click the "Analyze" button to initiate the analytics process.</li>
         </ol>
+        <img src="assets/img/Analyzepage.jpg" alt="Login Image">
 
         <h1 id="step03">
             <a href="#step03" class="header-anchor">#</a>
@@ -45,6 +47,7 @@
             <li><strong>4.1 Download PDF Reports:</strong> Extract valuable insights by downloading detailed PDF reports. This feature allows you to keep a record of your analytics findings.</li>
             <li><strong>4.2 Email Reports:</strong> Share your insights with stakeholders by clicking the "Email PDF" button. This facilitates easy communication and collaboration.</li>
         </ol>
+        <img src="assets/img/downloadreports.jpg" alt="Login Image">
 
         <h1 id="step05">
             <a href="#step05" class="header-anchor">#</a>
@@ -52,6 +55,7 @@
         </h1>
         <ol style="list-style-type: none;">
             <li><strong>5.1 Automate Reporting:</strong> Streamline your workflow by scheduling reports on a weekly or monthly basis. This ensures you stay updated without manual effort.</li>
+            <img src="assets/img/Schedulereports.jpg" alt="Login Image">
             <p>Smart Social's Analytics feature empowers you to make data-driven decisions, understand audience behavior, and optimize your social media strategy. Whether you need a detailed PDF report or want to share insights via email, Smart Social has you covered.</p>
             <p>If you have any questions or need further assistance, our support team is ready to guide you through the analytics journey.</p>
             <p>Happy analyzing with Smart Social!</p>
